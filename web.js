d3b4201864b059dd6ac2b7a62bba00f3ac902609
Eric Ulkloss <eulkloss@gmail.com>
2013-07-10 20:52:58 +0000

    changed web.js to point to index.html

--- web.js
+++ web.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var Hello = new Buffer(fs.readFileSync('index.html'), "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(Hello);
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
