1ef8a597091a483e0447112ffb9f42d0449026c5
Eric Ulkloss <eulkloss@gmail.com>
2013-07-20 04:02:26 +0000

    Responsive Design

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var express = require("express");
-
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-//var Hello = new Buffer(fs.readFileSync('index.html'), "utf-8");
-var Hello = fs.readFileSync('index.html', 'utf8');
-
 app.get('/', function(request, response) {
-  response.send(Hello);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
